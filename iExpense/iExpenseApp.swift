4633bdf59b99a8d05240a6863dd16e98d148ef92
karma <karmatashi954@gmail.com>
2022-05-10 16:31:54 +0545

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by karma on 5/10/22.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
